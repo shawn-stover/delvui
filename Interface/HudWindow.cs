8807bca3d626cf637c6c7b63f904556eabc6b6a6
Cliff <duartejhfaria@gmail.com>
2021-08-24 20:59:49 +0100

    feat: hp bars further on X axys

--- Interface/HudWindow.cs
+++ Interface/HudWindow.cs
@@ -21,7 +21,7 @@ namespace DelvUIPlugin.Interface {
 
         protected float CenterX => ImGui.GetMainViewport().Size.X / 2f;
         protected float CenterY => ImGui.GetMainViewport().Size.Y / 2f;
-        protected int XOffset => 160;
+        protected int XOffset => 200;
         protected int YOffset => 260;
         protected int BarHeight => 50;
         protected int BarWidth => 270;
